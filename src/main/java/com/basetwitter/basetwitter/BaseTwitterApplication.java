8038bf083764ebffb8502f8baf7e4f2fc0d6daee
Rafael Raposo <rafaelraposo@spotify.com>
2020-12-22 13:19:01 +0100

    First commit

---
+++ src/main/java/com/basetwitter/basetwitter/BaseTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.basetwitter.basetwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basetwitter/basetwitter/BaseTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basetwitter.basetwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
