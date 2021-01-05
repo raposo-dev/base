d5db640748f863921da2da941d6d59ed3bdf82de
Rafael Raposo <rafaeljraposo@gmail.com>
2021-01-05 12:51:10 +0100

    Refactored

--- src/main/java/com/base/base/service/TooManyContractsException.java
+++ src/main/java/com/base/base/service/TooManyContractsException.java
@@ -2,6 +2,11 @@ package com.base.base.service;
 
 public class TooManyContractsException extends Exception {
 
+  /**
+   *
+   */
+  private static final long serialVersionUID = -415603425227797894L;
+
   public TooManyContractsException(String message) {
     super(message);
   }
