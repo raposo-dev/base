68db2fa8f39de7ea3f767977417c89be7cf23936
Rafael Ribeiro Raposo <rafaelraposo@spotify.com>
2021-11-28 09:46:40 +0000

    Fixed Contract Details

--- src/main/resources/db/migration/V1.2__newContractFields.sql
+++ src/main/resources/db/migration/V1.2__newContractFields.sql
@@ -2,5 +2,5 @@ ALTER TABLE contract_details
 ADD COLUMN cpvs_designation text,
 ADD COLUMN cpvs_type text,
 ADD COLUMN cpvs_value text,
-ADD COLUMN group_members text,
+ADD COLUMN aquisition_state_Member_ue text,
 ADD COLUMN info_aquisition_state_memberue text;
